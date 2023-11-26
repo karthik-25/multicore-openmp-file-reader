125d2431ac149621a4fdd26badb42ec0c7e7793c
Karthikeyan Shanmugam <karthikeyan.shanmugam25@gmail.com>
2023-11-26 02:28:08 -0500

    rand chunks

--- mmap.c
+++ mmap.c
@@ -12,6 +12,67 @@
 #define BUFFER_SIZE 1024
 #define CHUNK_SIZE 1024
 
+int isNumberPresent(long arr[], int size, long target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  // Number is present
+        }
+    }
+    return -1;  // Number is not present
+}
+
+double findMax(double arr[], int size) {
+    if (size <= 0) {
+        // Handle empty array or invalid size
+        printf("Invalid array size.\n");
+        return -1;  // Returning a special value to indicate an error
+    }
+
+    double max = arr[0];  // Assume the first element is the maximum
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];  // Update the maximum if a larger element is found
+        }
+    }
+
+    return max;
+}
+
+double findMin(double arr[], int size) {
+    if (size <= 0) {
+        // Handle empty array or invalid size
+        printf("Invalid array size.\n");
+        return -1;  // Returning a special value to indicate an error
+    }
+
+    double min = arr[0];  // Assume the first element is the minimum
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];  // Update the minimum if a smaller element is found
+        }
+    }
+
+    return min;
+}
+
+double findAverage(double arr[], int size) {
+    if (size <= 0) {
+        // Handle empty array or invalid size
+        printf("Invalid array size.\n");
+        return -1.0;  // Returning a special value to indicate an error
+    }
+
+    double sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];  // Sum up all the elements in the array
+    }
+
+    return (double)sum / size;  // Calculate the average
+}
+
 int main(int argc, char *argv[]) {
     const char* filepath = argv[1];
     int t = atoi(argv[2]);
@@ -24,6 +85,12 @@ int main(int argc, char *argv[]) {
     int num_chunks = sb.st_size / CHUNK_SIZE + (sb.st_size % CHUNK_SIZE == 0 ? 0 : 1);
     printf("file size: %d\n", sb.st_size);
     printf("num of chunks: %d\n", num_chunks);
+
+    long rand_chunks[10];
+    double rand_chunks_time[10];
+    for(int x=0;x<10;x++){
+        rand_chunks[x] =(long) rand() % num_chunks + 1;
+    }
 
     // start timer
     start = omp_get_wtime();
@@ -38,15 +105,33 @@ int main(int argc, char *argv[]) {
         char *chunk_start = file_data + i * CHUNK_SIZE;
         int cur_chunk_size = (i == num_chunks - 1) ? sb.st_size - i * CHUNK_SIZE : CHUNK_SIZE;
         
+        // for testing correctness
         for (int j = 0; j < cur_chunk_size; j++) {
             read_counter++;
         }
+
+        int idx = isNumberPresent(rand_chunks, sizeof(rand_chunks) / sizeof(rand_chunks[0]), i);
+        if(idx!=-1){
+            rand_chunks_time[idx] = omp_get_wtime() - start;
+        }
+        
     }
     end = omp_get_wtime();
 
     printf("Total read: %d\n", read_counter);
     printf("Execution time: %f\n", end-start);
 
+
+    printf("Rand Chunks selected: ");
+    for (int i = 0; i < size; i++) {
+        printf("%ld ", rand_chunks[i]);
+    }
+    printf("\n");
+
+    printf("STATS: Average Response Time:%f\nMax Response Time:%f\nMin Response Time:%f\n", findAverage(rand_chunks_time, size), findMax(rand_chunks_time, size), findMin(rand_chunks_time, size));
+
+    munmap(file_data, sb.st_size);
     close(fd);
+
     return 0;
 }
