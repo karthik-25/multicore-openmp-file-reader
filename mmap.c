9e0550944ba955147d2977f2f71691be83e6b752
Karthikeyan Shanmugam <karthikeyan.shanmugam25@gmail.com>
2023-11-26 02:15:33 -0500

    updates

--- mmap.c
+++ mmap.c
@@ -16,12 +16,13 @@ int main(int argc, char *argv[]) {
     const char* filepath = argv[1];
     int t = atoi(argv[2]);
     double start, end;
-    // int global_counter = 0;
+    int read_counter = 0;
 
     // get file size and calculate number of chunks
     struct stat sb;
     stat(filepath, &sb);
     int num_chunks = sb.st_size / CHUNK_SIZE + (sb.st_size % CHUNK_SIZE == 0 ? 0 : 1);
+    printf("file size: %d\n", sb.st_size);
     printf("num of chunks: %d\n", num_chunks);
 
     // start timer
@@ -32,21 +33,18 @@ int main(int argc, char *argv[]) {
     char* file_data = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
     // read file chunks in parallel
-    #pragma omp parallel for num_threads(t)// reduction(+:global_counter)
+    #pragma omp parallel for num_threads(t) reduction(+:read_counter)
     for (int i = 0; i < num_chunks; i++) {
         char *chunk_start = file_data + i * CHUNK_SIZE;
         int cur_chunk_size = (i == num_chunks - 1) ? sb.st_size - i * CHUNK_SIZE : CHUNK_SIZE;
         
-        // for (int j = 0; j < cur_chunk_size; j++) {
-        //     if (*(chunk_start + j) == 'A') {
-        //         global_counter++;
-        //     }
-        // }
-        // fprintf(stdout, "Thread: %d | first char: %c | chunk size: %d | last char: %c\n", omp_get_thread_num(), *chunk_start, (int) cur_chunk_size, *(chunk_start + cur_chunk_size - 1));
+        for (int j = 0; j < cur_chunk_size; j++) {
+            read_counter++;
+        }
     }
     end = omp_get_wtime();
 
-    // printf("There are %d 'A's in the file.\n", global_counter);
+    printf("Total read: %d\n", read_counter);
     printf("Execution time: %f\n", end-start);
 
     close(fd);
